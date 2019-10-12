c073bcc82454143fe05d818507eab61b792512d3
berangeresn <webscribeo@gmail.com>
2019-10-12 18:01:52 +0200

    did cards

--- lib/pages/resultsSearch.dart
+++ lib/pages/resultsSearch.dart
@@ -18,24 +18,62 @@ class _ResultsSearchPageState extends State<ResultsSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        drawer: AppDrawer(),
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              title: Text(title),
-              floating: true,
-              flexibleSpace: Placeholder(),
-              expandedHeight: 150,
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                    (context, index) => Card(margin: EdgeInsets.all(20.0),child: Text('Item #$index')),
-                childCount: 41,
+      backgroundColor: Colors.white,
+      drawer: AppDrawer(),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text(title),
+            floating: true,
+            pinned: true,
+            flexibleSpace: Image.asset('images/technology.jpg', fit: BoxFit.cover,),
+            expandedHeight: 150,
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) => Container(
+                margin: EdgeInsets.all(10.0),
+                child: InkWell(
+                  splashColor: Colors.purple,
+                  onTap: () {
+                    /// Navigator.of(context).pushNamed('/cooperators/id');
+                  },
+                  child: Card(
+                    margin: EdgeInsets.all(30.0),
+                    elevation: 10.0,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0),
+                    ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        /// TODO : HERO WIDGET
+                        Container(
+                          height: 150.0,
+                          width: 50.0,
+                          margin: EdgeInsets.all(10.0),
+                          child: Placeholder(),
+                        ),
+                        Text('Coopérateur #$index', textAlign: TextAlign.center,textScaleFactor: 1.5,),
+                        Container(
+                          margin: EdgeInsets.all(10.0),
+                            alignment: Alignment.center,
+                            child: Text('Activité', textScaleFactor: 1.2,)),
+                        Container(
+                            margin: EdgeInsets.all(5.0),
+                            padding: EdgeInsets.all(5.0),
+                            alignment: Alignment.center,
+                            child: Text('Ville', textScaleFactor: 1.1,))
+                      ],
+                    ),
+                  ),
+                ),
               ),
+              childCount: 41,
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
